a80f169c65b863df451b5a1ae71db9231484dbf0
CodyS <codymoto122@gmail.com>
2024-01-21 20:29:37 -0500

    yikes

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: process.env.NODE_ENV === "production" ? "": undefined,
-    experimental: {
-        appDir: true,
-  },
-    images: {
-        unoptimized: true,
-  },
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
