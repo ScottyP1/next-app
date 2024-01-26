da2f06d3993cc6a19fbe9358eae48706ab9b489a
CodyS <codymoto122@gmail.com>
2024-01-25 22:08:20 -0500

    update

--- app/browse/[slug]/page.js
+++ app/browse/[slug]/page.js
@@ -5,6 +5,7 @@ import { getMovie } from "../../../lib/users";
 
 export default function MovieDetails({ params }) {
     const movie = getMovie(params.slug);
+    console.log(movie)
     if (!movie) {
         notFound();
     };
--- components/browsePage/ItemSections.js
+++ components/browsePage/ItemSections.js
@@ -3,9 +3,10 @@ import Image from "next/image";
 import GetData from '../../lib/users';
 import Link from "next/link";
 
-export default async function ItemSections({ caption }) {
-    const data = await GetData();
 
+
+export default async function ItemSections({ caption }) {
+    const data = GetData();
     return (
         <div className="mx-auto flex flex-col justify-center mt-6">
             <h1 className="text-white">{caption}</h1>
@@ -14,10 +15,10 @@ export default async function ItemSections({ caption }) {
                     {data.map((item, idx) => {
                         return (
                             <div key={idx} className="shrink-0">
-                                <Link href={`/browse/${item.slug}`}>
+                                <a href={`/browse/${item.slug}`}>
                                     <Image src={`/images/${item.imageCard}`} alt="" width={170} height={100} className="md:w-[300px]" />
                                     <p className="text-center text-[12px] md:text-[20px]">{item.title}</p>
-                                </Link>
+                                </a>
                             </div>
                         )
                     })}
