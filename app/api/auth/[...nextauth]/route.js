72394e017eb31c6eefe4d1aad7e5e684dfe2c177
CodyS <codymoto122@gmail.com>
2024-01-25 20:10:28 -0500

    update

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,6 +10,10 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
         CredentialsProvider({
             name: 'credentials',
             credentials: {
