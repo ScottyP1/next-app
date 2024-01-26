58e55c9a8995a4fc0ba8c2675fe507c9928645ca
CodyS <codymoto122@gmail.com>
2024-01-25 20:46:54 -0500

    update

--- components/browsePage/ItemSections.js
+++ components/browsePage/ItemSections.js
@@ -11,9 +11,9 @@ export default function ItemSections({ caption }) {
             <h1 className="text-white">{caption}</h1>
             <div>
                 <div className="flex flex-row-4 gap-2 overflow-y-auto">
-                    {data.map(item => {
+                    {data.map((item, idx) => {
                         return (
-                            <div key={item.title} className="shrink-0">
+                            <div key={idx} className="shrink-0">
                                 <Link href={`/browse/${item.slug}`}>
                                     <Image src={`/images/${item.imageCard}`} alt="" width={170} height={100} className="md:w-[300px]" />
                                     <p className="text-center text-[12px] md:text-[20px]">{item.title}</p>
