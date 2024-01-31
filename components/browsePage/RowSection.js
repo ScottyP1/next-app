a24c1b88c7bfc46ff8bd666303818a5c0cdc92ce
CodyS <codymoto122@gmail.com>
2024-01-30 19:33:16 -0500

    update

--- components/browsePage/RowSection.js
+++ components/browsePage/RowSection.js
@@ -15,9 +15,9 @@ export default async function RowSection({ type }) {
             <div className="mx-auto flex flex-col justify-center mt-6 ">
                 <h1 className="text-white text-[18px] md:text-[26px]">Popular on Netflix</h1>
                 <div>
-                    <div className="flex flex-row-4 gap-2 overflow-y-auto">
+                    <div className="flex flex-row-4 gap-2 overflow-y-auto ">
                         {data.map((item, idx) => (
-                            <div key={idx} className="shrink-0">
+                            <div key={idx} className="shrink-0 ">
                                 <Link href={`/browse/${item.slug}`}>
                                     <Image src={supabase.storage.from('NetflixImages').getPublicUrl(item.imageCard).data.publicUrl}
                                         alt="" width={170} height={100} className="md:w-[300px] h-[100px] md:h-[170px] rounded-md" />
