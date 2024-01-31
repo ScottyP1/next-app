2056ba85b973ce29b8563cb6e3aeb388c0e3550a
CodyS <codymoto122@gmail.com>
2024-01-30 20:00:05 -0500

    update

--- app/browse/page.js
+++ app/browse/page.js
@@ -1,16 +1,6 @@
-'use client'
 import LayoutPage from "@/components/browsePage/Layoutpage"
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-
 export default function Browse() {
-    const { status } = useSession();
-    if (status === "authenticated") {
-        return (
-            <LayoutPage title={'Grand Turismo'} type={'movie'} />
-        )
-    }
-    else if (status === "unauthenticated") {
-        redirect('/login')
-    }
+    return (
+        <LayoutPage title={'Grand Turismo'} type={'movie'} />
+    )
 }
