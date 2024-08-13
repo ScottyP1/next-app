14340ba3a1ec4848eaa244d727942662326bab59
Cody Scott <155593728+ScottyP1@users.noreply.github.com>
2024-08-13 18:53:38 -0400

    Update RowSection.js

--- components/browsePage/RowSection.js
+++ components/browsePage/RowSection.js
@@ -1,26 +1,39 @@
 import Row from "./Row";
 import supabase from "@/utils/supabase";
-
 import Link from "next/link";
 import Image from "next/image";
 
 export default async function RowSection({ type }) {
-    const { data } = await supabase
+    const { data, error } = await supabase
         .from('Movies')
         .select()
-        .eq('category', type)
+        .eq('category', type);
+
+    if (error) {
+        console.error("Error fetching data:", error);
+        return <div>Error loading data.</div>;
+    }
+
+    if (!data || data.length === 0) {
+        return <div>No data available for this category.</div>;
+    }
 
     return (
         <div className="relative top-[30px] md:top-[100px] w-screen">
             <div className="mx-auto flex flex-col justify-center mt-6 ">
                 <h1 className="text-white text-[18px] md:text-[26px]">Popular on Netflix</h1>
                 <div>
-                    <div className="flex flex-row-4 gap-2 overflow-y-auto ">
+                    <div className="flex flex-row-4 gap-2 overflow-y-auto">
                         {data.map((item, idx) => (
-                            <div key={idx} className="shrink-0 ">
+                            <div key={idx} className="shrink-0">
                                 <Link href={`/browse/${item.slug}`}>
-                                    <Image src={supabase.storage.from('NetflixImages').getPublicUrl(item.imageCard).data.publicUrl}
-                                        alt="" width={170} height={100} className="md:w-[300px] h-[100px] md:h-[170px] rounded-md" />
+                                    <Image 
+                                        src={supabase.storage.from('NetflixImages').getPublicUrl(item.imageCard).data.publicUrl} 
+                                        alt={item.title} 
+                                        width={170} 
+                                        height={100} 
+                                        className="md:w-[300px] h-[100px] md:h-[170px] rounded-md" 
+                                    />
                                 </Link>
                             </div>
                         ))}
@@ -31,5 +44,5 @@ export default async function RowSection({ type }) {
             <Row genre={'Drama'} data={data} />
             <Row genre={'Kids'} data={data} />
         </div>
-    )
+    );
 }
